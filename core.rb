30befff4b0328afb96bfeafbc869c87357d6acca
D1353L <meghanik400@gmail.com>
2016-05-25 16:55:22 +0300

    Refactoring. game.rb separated to console.rb

--- core.rb
+++ core.rb
@@ -3,7 +3,7 @@ class Core
   attr_accessor :secret_code
 
   def initialize
-     generate_code
+    generate_code
   end
 
   def generate_code
@@ -13,7 +13,6 @@ class Core
   def verify_code(code)
     raise 'Code size should be 4' unless code.size == 4
     raise 'Code digits should be in 1-6 range' if (code =~ /[1-6]{4}/).nil?
-
     result = ''
     secret_array = @secret_code.split('')
     code.chars.each_with_index do |dig, i|
@@ -22,7 +21,6 @@ class Core
         result[0, 0] = '+'
       end
     end
-
     code.chars.each_with_index do |dig, i|
       result << '-' if secret_array.include?(dig) && !secret_array[i].nil?
     end
--- game.rb
+++ game.rb
@@ -1,112 +1,37 @@
-require_relative 'core.rb'
 require 'yaml'
 
-#Game
+# Game
 class Game
-
   ATTEMPTS = 4
   HINTS = 1
 
+  attr_accessor :attempts, :hints, :score
   def initialize
-    @core = Core.new
-    p 'Welcome to Codebreaker app.'
-    p 'Enter your name:'
-    @name = gets.chomp
-  end
-
-  def start
     @attempts = ATTEMPTS
     @hints = HINTS
-    @core.generate_code
-    p 'Try to guess 4-digits (1-6) code:'
-    begin
-      begin
-        break if win?(@core.verify_code(get_user_input))
-      rescue RuntimeError => e
-        p e.message
-      end
-    end while @attempts > 0
-    save_score
-    new_game_or_exit
-  end
-
-  def get_user_input
-    if (input = gets.chomp) == 'hint'
-      hint
-      input = gets.chomp
-    end
-    input
   end
 
   def win?(result)
-    @win = false
-    if result == '++++'
-      p 'You win!'
-      @win = true
-    else
-      p result
-      @attempts -=1
-      if @attempts == 0
-        p 'Game over'
-      else
-        p "Failure. Left #{@attempts} attempts."
-      end
-    end
-    @win
+    win = false
+    result == '++++' ? win = true : @attempts -= 1
+    win
   end
 
-  def hint
+  def hint(secret_code)
     if @hints > 0
-      p @core.secret_code[rand(0..3)]
-      @hints -=1
-    else
-      p 'All hints used'
+      @hints -= 1
+      return secret_code[rand(0...secret_code.size)]
     end
+    nil
   end
 
-  def new_game_or_exit
-    begin
-      p 'Do you want to start new game? (y/n)'
-      answer = gets.chomp
-      start if answer == 'y'
-      if answer == 'n'
-        output_top10_scores
-        exit
-      end
-    end while answer.downcase != 'y' || answer.downcase != 'n'
-  end
-
-  def save_score
-    @score = {name: @name, code: @core.secret_code, attempts: ATTEMPTS - @attempts, win: @win ? :win : :lose}
+  def save_score(name, code, win)
+    @score = { name: name, code: code,
+               attempts: ATTEMPTS - @attempts, win: win ? :win : :lose }
     File.open('scores.yaml', 'a') { |f| f.puts @score.to_yaml }
   end
 
-  def get_scores
+  def load_scores
     YAML.load_stream(File.open('scores.yaml'))
   end
-
-  def output_top10_scores
-    scores = get_scores.sort_by { |hsh| hsh[:attempts] }
-
-    print '#'.rjust(5), 'Name |'.rjust(10), 'Code |'.rjust(10)
-    print 'Attempts |'.rjust(10), 'Win/lose |'.rjust(10), "\n"
-
-    scores[0..9].each_with_index do |hsh, i|
-      print "#{i+1}.".rjust(5)
-      hsh.each_value do |val|
-        print "#{val} |".rjust(10)
-      end
-      print "\n"
-    end
-
-    if scores.index(@score) > 9
-      print '......'.center(44), "\n"
-      print "#{scores.index(@score)+1}.".rjust(5)
-      @score.each_value do |val|
-        print "#{val} |".rjust(10)
-      end
-    end
-  end
 end
-
-Game.new.start
