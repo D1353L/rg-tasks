6e67a922075ae0407940f134be49970cdb0e088a
D1353L <meghanik400@gmail.com>
2016-05-27 23:31:00 +0200

    Gem fixed

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,6 +1,6 @@
-require 'codebreaker/code'
-require 'codebreaker/game'
-require 'codebreaker/console'
+require_relative 'codebreaker/code'
+require_relative 'codebreaker/game'
+require_relative 'codebreaker/console'
 
 module Codebreaker
   # Your code goes here...
